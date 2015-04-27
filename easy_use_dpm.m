115e756a9314325afef16359321bef7bbdfa1901
rocbomb <qzly7187007@gmail.com>
2015-04-27 21:21:08 +0800

    run dpm

---
+++ easy_use_dpm.m
@@ -0,0 +1,9 @@
+function easy_use_dpm(im)
+
+    model = load('../voc-release4.01/VOC2007/bicycle_final');
+    model=model.model;
+    cls = model.class;
+
+    [dets, boxes,flag ]= rundpm(im,model,cls);
+
+end
